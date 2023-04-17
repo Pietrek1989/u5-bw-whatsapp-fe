6d4eb00a98cef73824336fad89401c05996eca3c
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-04-17 12:35:32 +0200

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./components/MainPage";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar></Navbar>
+        <Routes>
+          <Route element={<MainPage />} path="/main" />
+          <Route element={<Login />} path="/" />
+          <Route element={<Register />} path="/register" />
+        </Routes>
+      </div>
+      <Footer></Footer>
+    </BrowserRouter>
+  );
 }
 
 export default App;
