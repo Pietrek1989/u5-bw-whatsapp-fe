88c08b7165bb5580e6d8bd3143da3e18437b99db
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-04-17 13:16:30 +0200

    Working on log in

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Navbar></Navbar>
-        <Routes>
-          <Route element={<MainPage />} path="/main" />
-          <Route element={<Login />} path="/" />
-          <Route element={<Register />} path="/register" />
-        </Routes>
+      <div className="App" id="parent-container">
+        <div id="top-bg"></div>
+        <div id="lower-bg">
+          <Routes>
+            <Route element={<MainPage />} path="/main" />
+            <Route element={<Login />} path="/" />
+            <Route element={<Register />} path="/register" />
+          </Routes>
+        </div>
       </div>
-      <Footer></Footer>
     </BrowserRouter>
   );
 }
