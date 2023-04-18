5c1093b4bad29b791c5bea1d6feeb47a00dee41d
Florian Dreyer <flodreyer@web.de>
2023-04-18 16:07:00 +0200

    fetching all chats

--- src/components/Sidebar/ContactItem.tsx
+++ src/components/Sidebar/ContactItem.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import "../../styles/ContactItem.css"
+import { Chat } from '../../interfaces';
+import { useAppDispatch } from '../../redux/hooks';
+import { setActiveChat } from '../../redux/reducers';
 
-interface Contact {
-    id: string;
-    name: string;
-    avatar?: string;
-}
+const ContactItem: React.FC<{chat: Chat}> = (props) => {
+    const dispatch = useAppDispatch()
 
-interface ContactItemProps {
-    contact: Contact;
-}
-
-const ContactItem: React.FC<ContactItemProps> = ({ contact }) => {
     return (
-        <div className="contact-item">
-            <img className="contact-avatar" src={contact.avatar} alt={contact.name} />
+        <div className="contact-item" onClick={() => dispatch(setActiveChat(props.chat._id))}>
+            <img className="contact-avatar" src={props.chat.members[1].avatar ? props.chat.members[1].avatar: "https://st3.depositphotos.com/4111759/13425/v/600/depositphotos_134255710-stock-illustration-avatar-vector-male-profile-gray.jpg"} alt={"bla"} />
             <div className="contact-details">
-                <h4 className="contact-name">{contact.name}</h4>
+                <h4 className="contact-name">{props.chat.members[1].name}</h4>
                 <p className="contact-last-message">
                     {/* Add the last message preview here */}
                     Last message text goes here...
